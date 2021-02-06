5e0b05c925b2a7e1cd132ec450d0f6b34963f401
Christopher Kim <chris.kim1990@gmail.com>
2021-02-06 00:49:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-  array << "arrays!"
+  element = "arrays!"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -18,25 +19,32 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  array = ["I", "am", "really", "learning", "arrays!"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = array[2]
 end
 
 def retrieve_first_element_from_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 4
+  array[4]  
 end
